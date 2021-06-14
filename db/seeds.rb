fb1817e1393f2a50100ce2e2af5e808df075ea18
Andrés Nuñez <andresanunez98@gmail.com>
2021-06-14 15:52:38 -0400

    uncommented cors.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Garden.destroy_all
+
+Plant.destroy_all
+
+plant_name = ["Petunia", "Marigold", "Monstera", "Calla Lilly", "Rose", "Mint", "Daisy", "Hydrangea"]
+plant_types = [ "Flower", "Tree", "Bush", "Herb"]
+
+
+10.times do
+
+    Garden.create(
+        name: Faker::Book.title , 
+        gardener_name: Faker::Artist.name, 
+        likes: rand(1..10000),   
+        location: Faker::WorldCup.city  
+    )
+
+end
+
+
+20.times do
+
+    Plant.create(name: plant_name.sample, 
+                plant_type: plant_types.sample,
+                image: Faker::Avatar.image ,
+                water_level: rand(1..10),
+                garden: Garden.all.sample,
+                        
+            )
+    end
+
+    puts ". . . SEEDS HAVE BEEN PLANTED :D . . ."
